3b3f06176c105e256e87487c3d2f4da0173f3920
Yahya08 <yahyahanda.r@students.amikom.ac.id>
2022-05-19 13:24:39 +0900

    UTS PEMROGRAMAN

---
+++ classprogram.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen1 = new Dosen(1, "Kamarudin, M.kom ", 21114408, "Laki-laki", "Pemrograman");
+
+            dosen1.cetakHasil();
+            Console.WriteLine();
+            dosen1.addDosen();
+            dosen1.addCourse();
+            Console.WriteLine();
+            dosen1.cetakAkhir();
+
+            Console.ReadKey();
+        }
+    }
+}
